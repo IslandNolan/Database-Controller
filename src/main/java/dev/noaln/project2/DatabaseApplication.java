5b9763d9f7d5cd322dff9cb5f621d13f62cbeb0f
IslandNolan <Nolanboner@icloud.com>
2021-12-07 16:50:35 -0500

    Rewrote again

--- src/main/java/dev/noaln/project2/DatabaseApplication.java
+++ src/main/java/dev/noaln/project2/DatabaseApplication.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
@@ -24,80 +25,53 @@ import java.io.IOException;
 
 
 public class DatabaseApplication extends Application {
-    private static String connection = "jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3307/team2";
-    private static String username="N01440422";
-    private static String pass = "Fall20210422";
+    //private static String connection = "jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3307/team2";
+    //private static String username="N01440422";
+    //private static String pass = "Fall20210422";
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private TextField address;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
+
     static Connection con=null;
 
 
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(DatabaseApplication.class.getResource("DatabasesScene.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-
-        Text t1 = new Text("Server Address:");
-        t1.setLayoutX(115);
-        t1.setLayoutY(115);
-        TextField serverAddress = new TextField("jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3307/team2");
-        serverAddress.setLayoutY(100);
-        serverAddress.setLayoutX(225);
-
-
-        TextField username = new TextField("N01440422");
-        username.setLayoutY(150);
-        username.setLayoutX(225);
-        Text t2 = new Text("Login Name:");
-        t2.setLayoutX(115);
-        t2.setLayoutY(165);
-
-
-        Text t3 = new Text("Password:");
-        t3.setLayoutX(115);
-        t3.setLayoutY(215);
-        TextField password = new TextField("");
-        password.setLayoutY(200);
-        password.setLayoutX(225);
-
-        Button b = new Button("Connect to Database.. ");
-        b.setLayoutX(225);
-        b.setLayoutY(250);
-
-        Group root = new Group();
-        root.getChildren().addAll(serverAddress,b,password,username,t1,t2,t3);
-
-
-        b.setOnAction(e -> {
-            connectButtonClicked(serverAddress,username,password);
-        });
-
-        Scene s = new Scene(root,640,480);
-
-
+        FXMLLoader fxmlLoader = new FXMLLoader(DatabaseApplication.class.getResource("DatabaseControllerFX.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage.setTitle("SQL Controller");
-        stage.setResizable(false);
-        stage.setScene(s);
+        stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) throws SQLException, MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         launch();
     }
-    public void connectButtonClicked(TextField address, TextField user, TextField pass) {
+
+    @FXML
+    public void onLoginButtonClicked(ActionEvent actionEvent) {
         try{
-            String addr = address.getText();
-            String us = user.getText();
-            String pas = pass.getText();
-            con = DriverManager.getConnection(addr, us, pas);
+
+            con = DriverManager.getConnection(address.getText(), username.getText(), password.getText());
             System.out.println("Connected to Database: [" + con.getCatalog() + "]");
 
             //We should launch the "Edit" view from this point because we have a good connection.
         }
         catch (Exception E){
             System.out.println("Something went wrong, Please try again! ");
-            pass.clear();
         }
 
     }
 
+    public void onContextMenuRequested(ContextMenuEvent contextMenuEvent) {
+    }
 }
