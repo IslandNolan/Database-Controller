830dde0e1aeddb527375f7c852aba6cf6184f219
IslandNolan <Nolanboner@icloud.com>
2021-12-08 22:27:18 -0500

    Implemented several new features:
    -ListView for pending database actions.
    -Select/Drop/SelectAll/DropAll Button functionality
    -Formatting fixes
    -Activated input fields for login page
    -Implement Popup for when resultSet is not null in next commit.

--- src/main/java/dev/noaln/project2/DatabaseApplication.java
+++ src/main/java/dev/noaln/project2/DatabaseApplication.java
@@ -1,6 +1,7 @@
 package dev.noaln.project2;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.VBox;
@@ -24,35 +26,34 @@ import java.sql.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class DatabaseApplication extends Application {
+
     private static String connection = "jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3307/team2";
     private static String usernames="N01440422";
     private static String pass = "Fall20210422";
 
 
-
-
-
+    private ArrayList<ResultSet> rs = new ArrayList<>();
     @FXML
-    private ArrayList<String> queryHistory = new ArrayList<>();
+    private ListView<Entry> executionPane;
+    @FXML
+    private ArrayList<Entry> queryHistory = new ArrayList<>();
     @FXML
     private TextField commandQuery;
     @FXML
     private Text actionCount;
-    @FXML
-    private Button applyQueries;
-    @FXML
-    private Button discardQueries;
-    @FXML
-    private Button loginButton;
     @FXML
     private TextField address;
     @FXML
     private TextField username;
     @FXML
     private TextField password;
+    @FXML
+    private Text connectedTo;
+
 
     static Stage stage = null;
     static Connection con=null;
@@ -64,13 +65,13 @@ public class DatabaseApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(DatabaseApplication.class.getResource("DatabaseLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         stage=s;
-        stage.setTitle("SQL Controller");
+        stage.setTitle("SQL Controller [Group 2: Nolan, CJ, Elizabeth, Parthi]");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) throws SQLException, MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-
         launch();
     }
     @FXML
@@ -81,49 +82,149 @@ public class DatabaseApplication extends Application {
         }
         String str = commandQuery.getText();
         commandQuery.clear();
-        queryHistory.add(str);
-        actionCount.setText("Actions Pending Execution "+queryHistory.size());
+        Entry current = new Entry(queryHistory.size(),str);
+        queryHistory.add(current);
+        executionPane.getItems().add(current); //We want to reverse this eventually
+
+        updateActionCount();
         commandQuery.setPromptText("Added SQL Query to pending list!");
+    }
+    public void updateActionCount(){
+        actionCount.setText("Actions Pending Execution: "+queryHistory.size());
+    }
+    @FXML
+    public void dropSelectedEntry(){
+        Entry E = executionPane.getSelectionModel().getSelectedItem();
+        executionPane.getItems().remove(E);
+        queryHistory.remove(E);
+        updateActionCount();
+        queryHistory.trimToSize();
+        System.out.println("Dropped Selected Statement: "+E.query);
+    }
+    @FXML
+    public void executeSelectedEntry(){
+        Entry E = executionPane.getSelectionModel().getSelectedItem();
+        Boolean b = execute(E);
+        if(b){
+            executionPane.getItems().remove(E); //if succeeds then remove
+            queryHistory.remove(E);
+            queryHistory.trimToSize();
+            System.out.println("Successfully Executed Query: "+E.query);
+        }
+        updateActionCount();
+    }
+    private boolean execute(Entry E){
+        try{
+            Statement s = con.createStatement();
+            s.execute(E.query);
+            System.out.println("Result Set: " + s.getResultSet());
+            return true;
+        }
+        catch(SQLException e){
+            System.out.println("An error has occurred while processing Statement: "+E.query);
+            return false;
+        }
     }
 
     @FXML
     public void onLoginButtonClicked(ActionEvent actionEvent) {
         try{
-            con = DriverManager.getConnection(connection, usernames, pass);
+            con = DriverManager.getConnection(address.getText(), username.getText(), password.getText());
             System.out.println("Connected to Database: [" + con.getCatalog() + "]");
         }
         catch (Exception E){
+            E.printStackTrace();
             System.out.println("Something went wrong, Please try again! ");
+            return;
         }
-
 
         //This will launch assuming everything is working properly.
         enterConnectedMode();
     }
     public void enterConnectedMode() {
         try{
+
             FXMLLoader fxmlLoader = new FXMLLoader(DatabaseApplication.class.getResource("DatabaseConnectedPage.fxml"));
             Scene scene = new Scene(fxmlLoader.load(),640,480);
             stage.hide();
             stage.setScene(scene);
             stage.show();
-
+            updateUsingDatabase();
         }
         catch (Exception E){
             System.out.println("Something went wrong! ");
 
         }
-
+    }
+    public void updateUsingDatabase() throws SQLException {
+        connectedTo.setText("Using Database: "+con.getCatalog());
     }
     @FXML
     public void onApplyQueriesClicked(ActionEvent actionEvent){
 
+        Iterator<Entry> itr = queryHistory.iterator();
+        while(itr.hasNext()){
+            Entry E = itr.next();
+            System.out.println("Success: ["+execute(E)+ "] for Statement: "+E.query);
+            executionPane.getItems().remove(E);
+            itr.remove();
+        }
+        updateActionCount();
     }
     @FXML
     public void onDiscardQueriesClicked(ActionEvent actionEvent){
-
+        queryHistory.clear();
+        executionPane.getItems().clear();
+        updateActionCount();
     }
     @FXML
+    public void onResetLoginClicked(){
+        try{
+            start(stage);
+            username.setText(usernames);
+            password.setText(pass);
+            address.setText(connection);
+        }
+        catch (Exception E){
+
+        }
+    }
+    @FXML
+    public void onDisconnect(){
+        try{
+            queryHistory.clear();
+            con.close();
+            con=null;
+            executionPane.getItems().clear();
+            queryHistory.clear();
+            executionPane.getItems().clear();
+            updateActionCount();
+            rs.clear();
+            start(stage);
+            System.gc();
+        }
+        catch  (Exception E){
+            E.printStackTrace();
+            System.out.println("Unable to Reset- An Error has occurred");
+        }
+    }
+
+    @FXML
     public void onContextMenuRequested(ContextMenuEvent contextMenuEvent) {
+    }
+    class Entry {
+        String query;
+        Integer index;
+        String errorMessage=null;
+        public Entry(Integer index, String query){
+            this.query = query;
+            this.index = index+1;
+        }
+        public String toString(){
+            return index+". "+query;
+        }
+        public void setErrorMessage(String s){
+            this.errorMessage=s;
+        }
     }
 }
